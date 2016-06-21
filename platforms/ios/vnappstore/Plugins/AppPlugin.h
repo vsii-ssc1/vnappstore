c2ac634e834eb7a6c5884ca62b98957a39ba24b2
Dam Than Siem <damthansiem@gmai.com>
2016-06-21 14:10:32 +0700

    - update

---
+++ platforms/ios/vnappstore/Plugins/AppPlugin.h
@@ -0,0 +1,13 @@
+//
+//  AppPlugin.h
+//  vnappstore
+//
+//  Created by Dam Than Siem on 6/21/16.
+//
+//
+
+#import <Cordova/Cordova.h>
+
+@interface AppPlugin : CDVPlugin
+
+@end
---
+++ platforms/ios/vnappstore/Plugins/AppPlugin.m
@@ -0,0 +1,13 @@
+//
+//  AppPlugin.m
+//  vnappstore
+//
+//  Created by Dam Than Siem on 6/21/16.
+//
+//
+
+#import "AppPlugin.h"
+
+@implementation AppPlugin
+
+@end
